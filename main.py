8c5d634931ccb99d5e7ebd53273bca48820e9efa
DavBaquero <david.baquero.19@gmail.com>
2025-11-17 07:51:58 +0100

    Feat(main.py): add appbar to main

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import flet as ft
 
+from Vist.appbar import create_appbar
+
 def main(page: ft.Page):
     page.title = "Flet Counter App"
     page.scroll = "auto"
+    page.appbar = create_appbar()
+    
+    texto = ft.Text("Para ver", size=30)
+
+    page.add(texto)
+    
 
 ft.app(target=main)
