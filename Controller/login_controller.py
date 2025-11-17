9f0ce04ab153b5e4acaa37b1c57558f6c4f8e1e9
DavBaquero <david.baquero.19@gmail.com>
2025-11-17 13:05:23 +0100

    Refactor: refactor login button

--- Controller/login_controller.py
+++ Controller/login_controller.py
@@ -1,15 +1,27 @@
 import flet as ft
 
-import View.user as user_view
+import View.home as home_view
 import View.register as register_view
 
-def login_button_clicked(e):
+def login_button_clicked(e, login_field, password_field):
     page = e.page
-    page.controls.clear()
-    sesion = True  
-    if sesion:
-        user_view.user_view(page)
-    page.update()
+    username_value = login_field.value
+    password_value = password_field.value
+    if not username_value:
+        login_field.error_text = "Por favor, ingresa un nombre de usuario"
+        page.update()
+        return
+    elif not password_value:
+        password_field.error_text = "Por favor, ingresa una contraseña"
+        login_field.error_text = None
+        page.update()
+        return
+    else:
+        login_field.error_text = None
+        password_field.error_text = None
+        page.controls.clear()
+        home_view.home_view(page)
+        page.update()
 
 def register_button_clicked(e):
     page = e.page
--- View/login.py
+++ View/login.py
@@ -11,7 +11,7 @@ def login_view(page: ft.Page):
     login_text = ft.Text("¿Tienes cuenta? Inicia sesión", size=25)
     login_field = ft.TextField(label="Usuario", width=300)
     password_field = ft.TextField(label="Contraseña", width=300, password=True, can_reveal_password=True)
-    login_button = ft.ElevatedButton(text="Iniciar Sesión", width=150, on_click=check_login)
+    login_button = ft.ElevatedButton(text="Iniciar Sesión", width=150, on_click=lambda e: login_controller.login_button_clicked(e,login_field,password_field))
     separator = ft.Divider(height=20)
     signup_text = ft.Text("¿No tienes cuenta? Regístrate", size=25)
     signup_button = ft.ElevatedButton(text="Registrarse", width=150, on_click=login_controller.register_button_clicked)
@@ -26,15 +26,3 @@ def login_view(page: ft.Page):
         signup_button
     )
     page.update()
-
-def check_login(e):
-    login_field = e.page.controls[1]
-    password_field = e.page.controls[2]
-    if login_field.value == "" or login_field.value is None: 
-        login_field.error_text = "Por favor, ingresa tu usuario"
-    elif password_field.value == "" or password_field.value is None:
-        login_field.error_text = None
-        password_field.error_text = "Por favor, ingresa tu contraseña"
-    else:
-        login_controller.login_button_clicked(e)
-    e.page.update()
