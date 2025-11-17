26dc7fa28f13c989cc86cf160a1613b6762bad4d
DavBaquero <david.baquero.19@gmail.com>
2025-11-17 10:52:15 +0100

    Build: Change structure

---
+++ View/appbar.py
@@ -0,0 +1,39 @@
+import flet as ft
+import View.user as user_view
+
+def create_appbar():
+    return ft.AppBar(
+        title=ft.Text("Reservas Galvintec"),
+        bgcolor=ft.Colors.BLUE_GREY_700,
+        actions=[
+            ft.IconButton(ft.Icons.WB_SUNNY_OUTLINED, on_click=change_theme),
+            ft.PopupMenuButton(
+                items=[
+                    ft.PopupMenuItem(text="Usuario", on_click=user_page),
+                    ft.PopupMenuItem(),
+                ],
+            ),
+        ],
+    )
+
+def change_theme(e):
+    page = e.page
+    if page.theme_mode == "light":
+        page.theme_mode = "dark"
+        e.control.icon = ft.Icons.NIGHTLIGHT_ROUND
+    else:
+        page.theme_mode = "light"
+        e.control.icon = ft.Icons.WB_SUNNY_OUTLINED
+    
+    if page.theme_mode == "light":
+        page.bgcolor = "#DBDADA"
+        page.appbar.bgcolor = "#575757"
+    else:
+        page.bgcolor = "#1B1B1B"
+        page.appbar.bgcolor = "#575757"
+    page.update()
+
+def user_page(e):
+    page = e.page
+    page.controls.clear()
+    user_view.user_view(page)
