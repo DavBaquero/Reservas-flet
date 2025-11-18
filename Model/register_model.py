2e050a6e8d534e9dd0a667324380d18cfaf36cbf
DavBaquero <david.baquero.19@gmail.com>
2025-11-18 08:07:56 +0100

    Feat(register_model.py): Create user and validate

---
+++ Model/register_model.py
@@ -0,0 +1,23 @@
+import Model.conexion as conexion
+
+def create_user(username, password):
+    conn = conexion.connection()
+    cursor = conn.cursor()
+    if validate_user(username, password):
+        cursor.close()
+        conn.close()
+        return False
+    cursor.execute("INSERT INTO usuarios (email, contra) VALUES (%s, %s)", (username, password))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def validate_user(username, password):
+    conn = conexion.connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM usuarios WHERE email = %s AND contra = %s", (username, password))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return user is not None
