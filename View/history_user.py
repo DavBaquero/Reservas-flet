68c6c9b5e83293328a4561b81439c31852d6763c
DavBaquero <david.baquero.19@gmail.com>
2025-11-19 10:55:14 +0100

    Feat(history_user.py): add button for save

--- View/history_user.py
+++ View/history_user.py
@@ -1,18 +1,20 @@
 import flet as ft
 
+import Controller.user_controller as user_controller
+
 def create_modal_dialog(reservations):
     rows = [
         ft.DataRow(
             cells=[
                 ft.DataCell(ft.Text(f"{r.get('fecha','')}/{r.get('hora','')}")),
-                ft.DataCell(ft.Text("Realizado" if r.get("estado") == 1 else "Cancelada", color=ft.Colors.GREEN if r.get("estado") == 1 else ft.Colors.RED)),
+                ft.DataCell(ft.Text("Realizado" if r.get("estado") == 1 else "Cancelada", color=ft.Colors.GREEN if r.get("estado") == 1 else ft.Colors.RED)), # Cambiarlo por un ENUM
                 ft.DataCell(ft.Text(r.get("tipo", ""))),
-                ft.DataCell(ft.Text(r.get("observaciones", ""))),
+                ft.DataCell(ft.TextField(r.get("obser", ""), multiline=True, expand=True)),
             ]
         )
         for r in reservations
     ]
-
+    
     data_table = ft.DataTable(
         columns=[
             ft.DataColumn(ft.Text("Fecha y Hora")),
@@ -22,24 +24,25 @@ def create_modal_dialog(reservations):
         ],
         rows=rows,
     )
-
     dialog = ft.AlertDialog(
         title=ft.Text("Historial de Reservas"),
         content=ft.Container(
             width=600,
             height=400,
-            content=ft.Column(
+            content=ft.Row(
                 expand=True,
-                scroll=ft.ScrollMode.AUTO,  # vertical
+                scroll=ft.ScrollMode.AUTO,  # horizontal
                 controls=[
-                    ft.Row(
+                    ft.Column(
+                        height=400,
+                        scroll=ft.ScrollMode.ALWAYS,  # vertical
                         controls=[data_table],
-                        scroll=ft.ScrollMode.AUTO,  # horizontal
                     )
                 ],
             ),
         ),
         actions=[
+            ft.ElevatedButton("Guardar", on_click=lambda e: guardar_obser(e, dialog)),
             ft.ElevatedButton("Cerrar", on_click=lambda e: cerrar_dialog(e, dialog)),
         ],
         actions_alignment=ft.MainAxisAlignment.END,
@@ -49,3 +52,17 @@ def create_modal_dialog(reservations):
 def cerrar_dialog(e,dialog):
     e.page.close(dialog)
     e.page.update()
+
+def guardar_obser(e, dialog):
+        try:
+            dt = dialog.content.content.controls[0].controls[0]
+            posicion = 0
+            for row in dt.rows:
+                if len(row.cells) >= 4:
+                    c = row.cells[3].content
+                    posicion += 1
+                    user_controller.actualizar_observacion(posicion, str(c.value or ""))
+            e.page.close(dialog)
+            e.page.update()
+        except Exception as ex:
+            print("No se pudo acceder a la tabla de datos.", ex)
