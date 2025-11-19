421d4f5350144fc4c90d16b73cdd824f9d981eb9
DavBaquero <david.baquero.19@gmail.com>
2025-11-19 14:11:59 +0100

    Fix(user.py): fix view

--- View/user.py
+++ View/user.py
@@ -12,41 +12,48 @@ def user_view(page: ft.Page):
     
     page.scroll = "hidden"
 
-    user_info_container = ft.Container(
-        content=ft.Column(
-            controls=[
-                ft.Text("Datos del Usuario", size=22, weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK),
-                ft.Divider(),
-                ft.Row(
+    user_info_container = ft.Row(
+        controls=[
+            ft.Container(
+                content=ft.Column(
                     controls=[
-                        ft.Text("Email:", weight=ft.FontWeight.W_600, width=50, color=ft.Colors.BLACK),
-                        ft.TextField(user_model.get_user(user_id)["email"], read_only=True, expand=1, color=ft.Colors.BLACK),
+                        ft.Text("Datos del Usuario", size=22, weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK),
+                        ft.Divider(),
+                        ft.Row(
+                            controls=[
+                                ft.Text("Email:", weight=ft.FontWeight.W_600, width=50, color=ft.Colors.BLACK),
+                                ft.TextField(user_model.get_user(user_id)["email"], read_only=True, expand=1, color=ft.Colors.BLACK),
+                            ],
+                        ),
+                        ft.Row(
+                            controls=[
+                                ft.Text("Estado:", weight=ft.FontWeight.W_600, width=120, color=ft.Colors.BLACK),
+                                ft.Text("Activo", expand=1, color=ft.Colors.GREEN_700),
+                            ]
+                        ),
+                        ft.Row(
+                            controls=[
+                                ft.Text("Reservas:", weight=ft.FontWeight.W_600, width=120, color=ft.Colors.BLACK),
+                                ft.ElevatedButton("Historial de reservas", expand=1, on_click=lambda e: user_controller.historial_reservas(e, user_id=user_id)),
+                            ]
+                        ),
                     ],
+                    spacing=20,
+                    alignment=ft.MainAxisAlignment.START,
+                    height=300,
+                    expand=True,
                 ),
-                ft.Row(
-                    controls=[
-                        ft.Text("Estado:", weight=ft.FontWeight.W_600, width=120, color=ft.Colors.BLACK),
-                        ft.Text("Activo", expand=1, color=ft.Colors.GREEN_700),
-                    ]
-                ),
-                ft.Row(
-                    controls=[
-                        ft.Text("Reservas:", weight=ft.FontWeight.W_600, width=120, color=ft.Colors.BLACK),
-                        ft.ElevatedButton("Historial de reservas", expand=1, on_click=lambda e: user_controller.historial_reservas(e, user_id=user_id)),
-                    ]
-                ),
-            ],
-            spacing=20,
-            alignment=ft.MainAxisAlignment.START,
-            height=300,
-            expand=True,
-        ),
-        padding=20,
-        bgcolor=ft.Colors.WHITE,
-        border=ft.border.all(1, ft.Colors.GREY_300),
-        border_radius=12,
-        shadow=ft.BoxShadow(blur_radius=8, spread_radius=1),
-        width=600,
+                alignment=ft.alignment.center,
+                padding=20,
+                bgcolor=ft.Colors.WHITE,
+                border=ft.border.all(1, ft.Colors.GREY_300),
+                border_radius=12,
+                shadow=ft.BoxShadow(blur_radius=8, spread_radius=1),
+                width=600,
+            )
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        expand=True,
     )
 
     page.add(user_info_container)
